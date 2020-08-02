ae0738ae603cc12b24e61fd99ea036437d6ea738
sajib581 <sajibdas581@gmail.com>
2020-08-02 11:04:02 +0600

    Timer 100% Complete

--- src/Components/Controller/Controller.js
+++ src/Components/Controller/Controller.js
@@ -1,25 +1,109 @@
-import React ,{Component} from "react"
+import React, { Component } from "react";
 
-class Controller extends Component{
-    constructor(props){
-        super(props)
+class Controller extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state={
-            start : true ,
-            pause : true,
-            lap : false ,
-            reset : false
-        }
-    }
-    getController(){
-        
-    }
+    this.state = {
+      start: true,
+      pause: true,
+      lap: false,
+      reset: false,
+    };
+  }
 
-    render(){
-        return(
-            <h1>Hello</h1>
-        )
+  startHandeler() {
+    this.setState({
+      ...this.state,
+      start: false,
+      pause: true,
+      lap: true,
+    });
+    this.props.start()
+  }
+
+  pauseHandeler() {
+    this.setState({
+      ...this.state,
+      start: true,
+      pause: false,
+      lap: false,
+      reset: true,
+    });
+    this.props.pause()
+  }
+
+  lapHandeler() {
+    this.props.lap()
+  }
+
+  resetHandeler() {
+    this.setState({
+      start: true,
+      pause: false,
+      lap: false,
+      reset: false,
+    });
+this.props.reset()         
+  }
+
+  getController() {
+    let output = null;
+
+    if (this.state.start && !this.state.reset) {
+      output = (
+        <div>
+          <button
+            className="btn btn-success btn-lg px-5 ml-5"
+            onClick={(event) => this.startHandeler()}
+          >
+            Start
+          </button>
+        </div>
+      );
+      
+    } else if (this.state.pause && this.state.lap) {
+      output = (
+        <div>
+          <button
+            className="btn btn-primary btn-lg px-5 ml-5"
+            onClick={(event) => this.pauseHandeler()}
+          >
+            Pause
+          </button>
+          <button
+            className="btn btn-warning btn-lg px-5 ml-5"
+            onClick={(event) => this.lapHandeler()}
+          >
+            Lap
+          </button>
+        </div>
+      );
+     
+    } else if (this.state.start && this.state.reset) {
+      output = (
+        <div>
+          <button
+            className="btn btn-success btn-lg px-5 ml-5"
+            onClick={() => this.startHandeler()}
+          >
+            Start
+          </button>
+          <button
+            className="btn btn-danger btn-lg px-5 ml-5"
+            onClick={() => this.resetHandeler()}
+          >
+            Reset
+          </button>
+        </div>
+      );
     }
+    return output;
+  }
+
+  render() {
+    return this.getController();
+  }
 }
 
-export default Controller
+export default Controller;
--- src/Components/CountDown/Digit/Digit.js
+++ src/Components/CountDown/Digit/Digit.js
@@ -4,7 +4,7 @@ const Digit = (props) => {
   return (
     <div
       className="Digit mx-3 text-center"
-      style={{ borderColor: props.color }}
+      style={{ borderColor: props.color }} 
     >
       <h1 className="display-2 py-3" style={{ color: props.color }}>
         {props.value<10?`0${props.value}`:props.value}
